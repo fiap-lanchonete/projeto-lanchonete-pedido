4b42e63e1d3a63f221b8403c1969e9cc41ff7a82
Pedro Marins <ph.marins@hotmail.com>
2024-01-10 22:02:01 -0300

    tests: adding coverage to category.controller

--- src/adapters/controllers/category/category.controller.spec.ts
+++ src/adapters/controllers/category/category.controller.spec.ts
@@ -59,6 +59,20 @@ describe('CategoryController', () => {
   });
 
   describe('createCategory', () => {
+    it('should throw generic error if is not a duplication error', async () => {
+      jest
+        .spyOn(mockCategoryService, 'create')
+        .mockImplementation(() =>
+          Promise.reject(new Error('Something went wrong')),
+        );
+
+      expect(
+        categoryController.createCategory({
+          name: 'Category 1',
+        }),
+      ).rejects.toThrow(Error);
+    });
+
     it('should retrieve 409 exception if category already exists', async () => {
       jest.spyOn(mockCategoryService, 'create').mockImplementation(() =>
         Promise.reject(
@@ -96,6 +110,56 @@ describe('CategoryController', () => {
           name: 'Category 1',
         }),
       ).toEqual(mockCategory);
+    });
+  });
+
+  describe('getCategory', () => {
+    it('should retrieve a category by id', async () => {
+      const mockCategory: Category = {
+        id: 1,
+        name: 'Category 1',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      jest
+        .spyOn(mockCategoryService, 'findOne')
+        .mockImplementation(() => Promise.resolve(mockCategory));
+
+      expect(await categoryController.getCategory('1')).toEqual(mockCategory);
+    });
+  });
+
+  describe('updateCategory', () => {
+    it('should update a category by id', async () => {
+      const mockCategory: Category = {
+        id: 1,
+        name: 'Category 1',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      jest
+        .spyOn(mockCategoryService, 'update')
+        .mockImplementation(() => Promise.resolve(mockCategory));
+
+      expect(
+        await categoryController.updateCategory('1', {
+          name: 'Category 1',
+        }),
+      ).toEqual(mockCategory);
+    });
+  });
+
+  describe('deleteCategory', () => {
+    it('should delete a category by id', async () => {
+      jest
+        .spyOn(mockCategoryService, 'delete')
+        .mockImplementation(() => Promise.resolve());
+
+      await categoryController.deleteCategory('1');
+      expect(mockCategoryService.delete).toHaveBeenCalledWith(1);
+      expect(mockCategoryService.delete).toHaveBeenCalledTimes(1);
     });
   });
 });
